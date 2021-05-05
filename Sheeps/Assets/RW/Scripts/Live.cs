623f7b2863059650c2cc241063cd03325f913334
sebastiaciudad <sebastia.ciudad@gmail.com>
2021-05-05 19:08:36 +0200

    Update Sheep project
    Update with the following changes:
     - Now we have a menu
     - Quit button added
     - You die when the life is 0

---
+++ Sheeps/Assets/RW/Scripts/Live.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Live : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public GameObject GameOverText;
+
+    private int cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        cont = 0;
+        GameOverText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Title");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        hearts[cont].SetActive(false);
+        cont = cont + 1;
+
+        if (cont >= 3)
+        {
+            //Time.timeScale = 0;
+            GameOverText.SetActive(true);
+            StartCoroutine(Wait());
+        }
+    }
+}
---
+++ Sheeps/Assets/RW/Scripts/StartGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1);
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
